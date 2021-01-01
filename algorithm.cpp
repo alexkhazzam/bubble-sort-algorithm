6ac0e78a789ccfea2a0da502ba01e0af3491e097
alexkhazzam <alex722khazzam@gmail.com>
2020-12-31 23:13:31 -0500

    refactoring

--- algorithm.cpp
+++ algorithm.cpp
@@ -3,11 +3,8 @@
 #include <string>
 #include <vector>
 
-using namespace std;
-
-int main()
+std::vector<double> sort(std::vector<double> numbers)
 {
-    std::vector<double> numbers{45, -5, 65, 87, 5.4, 23, 2, 1, 14, 14, 5};
     for (size_t i{0}; i < numbers.size(); ++i)
         for (size_t k{numbers.size() - 1}; k > i; --k)
             if (numbers[i] > numbers[k])
@@ -16,4 +13,23 @@ int main()
                 numbers[k] = numbers[i];
                 numbers[i] = cur_pos;
             }
+
+    return numbers;
+}
+
+int main()
+{
+    std::vector<double> unsorted_vec{45, -5, 65, 87, 5.4, 23, 2, 1, 14, 14, 5};
+    std::vector<double> sorted_vec{sort(unsorted_vec)};
+
+    std::cout << "[ ";
+    for (auto num : sorted_vec)
+        if (sorted_vec[sorted_vec.size() - 1] == num)
+            std::cout << " " << num << " ";
+        else
+            std::cout << " " << num << ", ";
+
+    std::cout << " ]" << std::endl;
+
+    return 0;
 }
