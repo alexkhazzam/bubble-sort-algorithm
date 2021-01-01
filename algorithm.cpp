12a3ba7f2052ebe81b5125c8e2086fef7ab90a4d
alexkhazzam <alex722khazzam@gmail.com>
2020-12-31 22:41:53 -0500

    adding doubles

--- algorithm.cpp
+++ algorithm.cpp
@@ -1,23 +1,19 @@
 #include <iostream>
 #include <iomanip>
-#include <cctype>
-#include <cstring>
 #include <string>
 #include <vector>
-#include <ctime>
-#include <cmath>
-#include <cstdlib>
-#include <climits>
+
+using namespace std;
 
 int main()
 {
-    std::vector<int> numbers{45, 65, 87, 23, 2, 1, 14, 14, 5};
+    std::vector<double> numbers{45, -5, 65, 87, 5.4, 23, 2, 1, 14, 14, 5};
 
-    for (unsigned int i{0}; i <= (numbers.size() - 1); ++i)
-        for (int k{static_cast<int>((numbers.size() - 1))}; k > i; --k)
+    for (double i{0}; i <= (numbers.size() - 1); ++i)
+        for (double k{static_cast<double>((numbers.size() - 1))}; k > i; --k)
             if (numbers[i] > numbers[k])
             {
-                int cur_pos{numbers[k]};
+                double cur_pos{numbers[k]};
                 numbers[k] = numbers[i];
                 numbers[i] = cur_pos;
             }
