bfb4d216366a3c282bef1a214dd3e5c2df99d487
alexkhazzam <alex722khazzam@gmail.com>
2020-12-31 23:14:40 -0500

    refactoring

--- algorithm.cpp
+++ algorithm.cpp
@@ -13,7 +13,6 @@ std::vector<double> sort(std::vector<double> numbers)
                 numbers[k] = numbers[i];
                 numbers[i] = cur_pos;
             }
-
     return numbers;
 }
 
@@ -21,15 +20,12 @@ int main()
 {
     std::vector<double> unsorted_vec{45, -5, 65, 87, 5.4, 23, 2, 1, 14, 14, 5};
     std::vector<double> sorted_vec{sort(unsorted_vec)};
-
     std::cout << "[ ";
     for (auto num : sorted_vec)
         if (sorted_vec[sorted_vec.size() - 1] == num)
             std::cout << " " << num << " ";
         else
             std::cout << " " << num << ", ";
-
     std::cout << " ]" << std::endl;
-
     return 0;
 }
