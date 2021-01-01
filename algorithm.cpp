06b34abbdcf5ef124d4db0ff63112e7c66eacd4d
alexkhazzam <alex722khazzam@gmail.com>
2020-12-31 22:47:29 -0500

    refactoring

--- algorithm.cpp
+++ algorithm.cpp
@@ -8,16 +8,12 @@ using namespace std;
 int main()
 {
     std::vector<double> numbers{45, -5, 65, 87, 5.4, 23, 2, 1, 14, 14, 5};
-
-    for (double i{0}; i <= (numbers.size() - 1); ++i)
-        for (double k{static_cast<double>((numbers.size() - 1))}; k > i; --k)
+    for (size_t i{0}; i < numbers.size(); ++i)
+        for (size_t k{numbers.size() - 1}; k > i; --k)
             if (numbers[i] > numbers[k])
             {
                 double cur_pos{numbers[k]};
                 numbers[k] = numbers[i];
                 numbers[i] = cur_pos;
             }
-
-    for (auto nums : numbers)
-        std::cout << nums << std::endl;
 }
